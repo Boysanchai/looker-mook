22f27b1ea9b3b5d9dcb72efca07cd1fc2f289bbb
Boysanchai <58928197+Boysanchai@users.noreply.github.com>
2025-11-25 17:44:03 +0700

    Create Stylesheet.js

---
+++ Stylesheet.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<style>
+  /* เพิ่ม CSS กำหนดเอง (Custom CSS) หากจำเป็น */
+  /* เช่น การตั้งค่า Font */
+  body {
+    font-family: 'Arial', sans-serif;
+  }
+</style>
