6748c4e20b44cd4c65ee3eaccdff8f87fc68363f
Boysanchai <58928197+Boysanchai@users.noreply.github.com>
2025-11-25 17:46:26 +0700

    Delete Stylesheet.js

--- Stylesheet.js
+++
@@ -1,8 +0,0 @@
-<script src="https://cdn.tailwindcss.com"></script>
-<style>
-  /* เพิ่ม CSS กำหนดเอง (Custom CSS) หากจำเป็น */
-  /* เช่น การตั้งค่า Font */
-  body {
-    font-family: 'Arial', sans-serif;
-  }
-</style>
